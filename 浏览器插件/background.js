6face56e42e1e172ced6b6ab50c8aac84467d2bb
PZ <709711356@qq.com>
2025-12-30 23:51:56 +0800

    标题修改

--- 浏览器插件/background.js
+++ 浏览器插件/background.js
@@ -18,9 +18,13 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === CONFIG.CONTEXT_MENU_ID) {
     const selectedText = info.selectionText;
+    const pageUrl = tab.url || info.pageUrl;
     
-    // 1. 将文字保存到 storage
-    chrome.storage.local.set({ selectedText: selectedText }, () => {
+    // 格式化文字：网址 + 摘抄：+ 具体的选中的文字
+    const formattedText = `${pageUrl} 摘抄：${selectedText}`;
+    
+    // 1. 将格式化后的文字保存到 storage
+    chrome.storage.local.set({ selectedText: formattedText }, () => {
       // 2. 打开侧边栏
       chrome.sidePanel.open({ windowId: tab.windowId });
     });
